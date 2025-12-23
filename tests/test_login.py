c2724e745f5414b817c1b823ad7229d90300c9a0
huyentran-space <ntht1204@gmail.com>
2025-12-23 11:10:18 +0700

    Refactor login tests for clarity and efficiency
    Removed redundant test case for failed login and simplified the success test case.

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from time import sleep
 import pytest
 
-def test_login_successfully_non_press_key(driver):
+def test_login_successfully(driver):
     driver.find_element(By.NAME, "username").send_keys("Admin")
     driver.find_element(By.NAME, "password").send_keys("admin123")
     #click on "Login" button
@@ -14,8 +14,4 @@ def test_login_successfully_non_press_key(driver):
     #Verify Dashboard tittle
    
     assert driver.find_element(By.CSS_SELECTOR, ".oxd-text--h6").text == "Dashboard"
-    
-def test_login_failed_wrongusername(driver):
-    driver.find_element(By.NAME, "username").send_keys("Admin")
-    driver.find_element(By.NAME, "password").send_keys("admin123")
     
