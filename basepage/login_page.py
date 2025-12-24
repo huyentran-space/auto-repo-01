0f7ec483ccdb635733949529668d5723432e353a
huyentran-space <ntht1204@gmail.com>
2025-12-24 21:18:11 +0700

    Implement LoginPage class for user login actions

--- basepage/login_page.py
+++ basepage/login_page.py
@@ -1,4 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import pytest
-from time import sleep
+from selenium.webdriver.common.keys import Keys    
+
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.username = (By.NAME, "username")
+        self.password = (By.NAME, "password")
+        self.login_button = (By.XPATH, "//button[@type='submit']")
+       
+    def enter_username(self, username):
+        self.driver.find_element(*self.username).send_keys(username)
+       
+    def enter_password(self, password):
+        self.driver.find_element(*self.password).send_keys(password)
+       
+    def click_login_button(self):
+        self.driver.find_element(*self.login_button).click()
+       
+    def do_login(self, username, password):
+        self.enter_username(username)
+        self.enter_password(password)
+        self.click_login_button()
