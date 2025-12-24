cd9f1c9f351cf3d4f50a9fa986152c9286b276b7
huyentran-space <ntht1204@gmail.com>
2025-12-24 21:21:39 +0700

    Refactor login tests by removing redundancy
    Removed redundant test_login_successfully function and cleaned up test_login.

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,19 +1,7 @@
 from pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
+
 def test_login(driver):
     login_page = LoginPage(driver)
     login_page.do_login("Admin","admin123")
-
-
-    assert driver.find_element(By.CSS_SELECTOR, ".oxd-text--h6").text == "Dashboard"
-def test_login_successfully(driver):
-    driver.find_element(By.NAME, "username").send_keys("Admin")
-    driver.find_element(By.NAME, "password").send_keys("admin123")
-    #click on "Login" button
-    driver.find_element(By.CSS_SELECTOR, ".orangehrm-login-button").click() #.orangehrm-login-button / .oxd-button
-   
-    sleep(5)
-    #Expected result: redirect to HomePage
-    #Verify Dashboard tittle
-   
     assert driver.find_element(By.CSS_SELECTOR, ".oxd-text--h6").text == "Dashboard"
