b1e70a5134b1a1d7279edf0e5eab2587274ad1a3
HuyenTran <ntht1204gmail.com>
2026-01-20 19:58:56 +0700

    Commit 3

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,13 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from utils.config_reader import ConfigReader
 
 @pytest.fixture(scope="function")
 def driver():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(ConfigReader.get_timeouts())
     driver.maximize_window()
     driver.get(ConfigReader.get_base_url())
