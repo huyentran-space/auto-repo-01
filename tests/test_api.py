88afd35a6b1e12eeafa05bd1ed2b5f59d47962ab
huyentran-space <ntht1204@gmail.com>
2026-01-20 16:37:45 +0700

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,8 +5,8 @@ from utils.config_reader import ConfigReader
 class TestAPI:
 
     def test_api_get(self):
-        self.api = APIHelper(ConfigReader.get("api_base_url"))
+        self.api = APIHelper(ConfigReader.get_api_base_url())
         response = self.api.api_get("posts/1/comments",params=None)
-        assert response["id"] == "2"
-        assert response["name"] == "quo vero reiciendis velit similique earum"
-        assert response["email"] == "Jayne_Kuhic@sydney.com"
+        assert response[1]["id"] == 2
+        assert response[1]["name"] == "quo vero reiciendis velit similique earum"
+        assert response[1]["email"] == "Jayne_Kuhic@sydney.com"
